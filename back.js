4325596f4f5d1995d0891b08e3e407ea31ef9ab9
Mateus Sanchez <mateo705@hotmail.com>
2024-08-12 14:50:14 -0500

    Se termina el Backend del proyecto

--- back.js
+++ back.js
@@ -1,41 +1,26 @@
-
 var resultado;
 var texto;
-var texto_total;
-
-
 
 inicial();
-function inicial(){
-    ocultar('encriptar')
-    mostrar('mensaje')
+function inicial() {
+    ocultar('encriptar');
+    mostrar('mensaje');
     document.getElementById('texto').focus();
 }
 
+function encriptar() {
+    texto = document.getElementById('texto').value;
+    resultado = checkType(texto);
 
-//PARAMETROS PARA ENCRIPTAR
-// La letra "e" es convertida para "enter"
-// La letra "i" es convertida para "imes"
-// La letra "a" es convertida para "ai"
-// La letra "o" es convertida para "ober"
-// La letra "u" es convertida para "ufat"
-
-function encriptar(){
-    texto= document.getElementById('texto').value;
-    resultado = checkType(texto)
-
-    //Comprobar si el campo Texto esta vacido
-
-    if (texto=='') {
-        mostrarAdvertencia("El campo esta vacido, digitaliza algo.");
+    if (texto === '') {
+        mostrarAdvertencia("El campo está vacío, digitaliza algo.");
         inicial();
-    } else if (resultado == '1' || resultado == '2') {
-        mostrarAdvertencia("Estas ingresando caracteres no validos.")
+    } else if (resultado === '1' || resultado === '2') {
+        mostrarAdvertencia("Estás ingresando caracteres no válidos.");
         inicial();
-
-    } else{
-        mostrar('encriptar'); 
-        ocultar('mensaje')
+    } else {
+        mostrar('encriptar');
+        ocultar('mensaje');
 
         const chars = {
             'a': 'ai',
@@ -44,19 +29,39 @@ function encriptar(){
             'o': 'ober',
             'u': 'ufat'
         };
-        let encripta = texto.replace(/[aeiou]/g, m => chars[m]);
-        asignarTextoElemento('.salida', encripta);
-        
-
+        let encriptar = texto.replace(/[aeiou]/g, m => chars[m]);
+        asignarTextoElemento('.salida', encriptar);
     }
-
 }
 
+function desencriptar() {
+    texto = document.getElementById('texto').value;
+    resultado = checkType(texto);
 
+    if (texto === '') {
+        mostrarAdvertencia("El campo está vacío, digitaliza algo.");
+        inicial();
+    } else if (resultado === '1' || resultado === '2') {
+        mostrarAdvertencia("Estás ingresando caracteres no válidos.");
+        inicial();
+    } else {
+        mostrar('encriptar');
+        ocultar('mensaje');
 
+        const reverse = {
+            'ai': 'a',
+            'enter': 'e',
+            'imes': 'i',
+            'ober': 'o',
+            'ufat': 'u',
+        };
 
+        const reverseRegEx = new RegExp(Object.keys(reverse).join('|'), 'g');
+        let desencriptar = texto.replace(reverseRegEx, m => reverse[m]);
+        asignarTextoElemento('.salida', desencriptar);
+    }
+}
 
-// Mostrar notificación de error
 function mostrarAdvertencia(message) {
     Swal.fire({
         icon: 'warning',
@@ -67,33 +72,55 @@ function mostrarAdvertencia(message) {
     });
 }
 
-
-//Funcion para detectar si hay mayusculas o numeros
 function checkType(mensaje) {
-    mensaje = String(mensaje).replace(/[" "]/g,"");
+    mensaje = String(mensaje).replace(/[" "]/g, "");
     mensaje = mensaje.replace(/\n/g, '');
     mensaje = String(mensaje).trim();
     const regxs = {
-      "lower": /^[a-z]+$/,
-      "upper": /^[A-Z0-9ÁÉÍÓÚÑÜ!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/,
-      "upperLower":  /^[A-Za-z0-9ÁÉÍÓÚÑÜáéíóúñü!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/
-    }; 
+        "lower": /^[a-z]+$/,
+        "upper": /^[A-Z0-9ÁÉÍÓÚÑÜ!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/,
+        "upperLower": /^[A-Za-z0-9ÁÉÍÓÚÑÜáéíóúñü!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/
+    };
     if (regxs.lower.test(mensaje)) {
         return '0';
-      }
-   
-      if (regxs.upper.test(mensaje)){
-       return '1';
-      }
-   
-      if (regxs.upperLower.test(mensaje)){
-       return '2';
-      }
-      
-      return -1;
-  }
+    }
 
-  function asignarTextoElemento(elemento, texto) {
+    if (regxs.upper.test(mensaje)) {
+        return '1';
+    }
+
+    if (regxs.upperLower.test(mensaje)) {
+        return '2';
+    }
+
+    return -1;
+}
+
+function copy (){
+    texto = document.querySelector('.salida');
+    texto.select();
+    document.execCommand('copy');
+    inicial();
+    limpiarCaja('#texto')
+       Swal.fire({
+        icon: 'success',
+        title: 'Copiado',
+        text: 'El texto ha sido copiado al portapapeles.',
+        confirmButtonColor: '#3085d6',
+        confirmButtonText: 'Cerrar',
+        timer: 3000,  // Cierra automáticamente después de 3000 milisegundos (3 segundos)
+        timerProgressBar: true  // Muestra una barra de progreso para el temporizador
+    });
+    
+
+}
+function limpiarCaja(caja){
+    document.querySelector(caja).value='';
+    return;
+     }
+
+
+function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     if (elementoHTML) {
         elementoHTML.value = texto;
@@ -102,9 +129,10 @@ function checkType(mensaje) {
     }
 }
 
-function mostrar(objeto){
+function mostrar(objeto) {
     document.getElementById(objeto).style.display = 'flex';
 }
-function ocultar(objeto){
+
+function ocultar(objeto) {
     document.getElementById(objeto).style.display = 'none';
 }
